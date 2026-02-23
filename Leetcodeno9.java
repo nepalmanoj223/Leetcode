c7c63ba38c8da6477f80612c3d26c0756a25a431
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-23 20:22:03 +0545

    Add LeetCode #9 Palindrome Number solution
    Add Leetcodeno9.java containing a Solution class with isPalindrome(int). The method reverses the integer via digit-by-digit arithmetic and compares the reversed value to the original to determine if it's a palindrome. File is placed in the Leetcode package.

---
+++ Leetcodeno9.java
@@ -0,0 +1,17 @@
+package Leetcode;
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n=x;
+        int nw=0;
+        while(x>0){
+            nw=nw*10 +x%10;
+            x=x/10;
+        }
+        if(nw==n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
