0cb5b505d2789d6ab442297308bc89cc53c421db
Manoj Nepal <nepalmanoj223@gmail.com>
2026-03-04 19:40:12 +0545

    this is te changed one which accepts all test cases the val is used to make sure it doesnot exceed time

--- Leetcode1095.java
+++ Leetcode1095.java
@@ -1,72 +1,74 @@
-public class Leetcode1095 {
-    public static void main(String[] args) {
-        int [] nums={1,2,3,4,5,3,1};
-        int target=5;
-        int ans=peak(nums,target);
-        System.out.println(ans);
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
 
-    }
-    static int peak(int []nums, int target){
-        int st=0;
-        int en=nums.length-1;
-        int ans=0;
-        int ls=st;
-        int le=0;
-        int rs=0;
-        int re=en;
-    
-        while(st<en){
-            int m=st+(en-st)/2;
-            if(nums[m]>nums[m+1]){
-                en=m;
-            }
-            else{
-                st=m+1;
-            }
-             
-        }
-        ans=st;
-        if(nums[ans]==target){
-            return ans;
-        }
-        else {
-            le=ans;
-            while(ls<=le){
-                int lm=ls+(le-ls)/2;
-                if(nums[lm]==target){
-                    return lm;
-                }
-                else if(nums[lm]<target){
-                    ls=lm+1;
-                }
-                else{
-                    le=lm-1;
-                }
-            }rs=ans;
-            while(rs<=re){
-                int rm=rs+(re-rs)/2;
-                if(nums[rm]==target){
-                    return rm;
-                }
-                else if(nums[rm]<target){
-                    rs=rm+1;
-                }
-                else{
-                    re=rm-1;
-                }
-            }
-        }
-       
-        return-1;
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
         
+        int st = 0;
+        int en = mountainArr.length() - 1;
+        int ans = 0;
+        int ls = st;
+        int le = 0;
+        int rs = 0;
+        int re = en;
+
+    
+        while (st < en) {
+            int m = st + (en - st) / 2;
+            int midVal = mountainArr.get(m);
+            int midNext = mountainArr.get(m + 1);
+            if (midVal > midNext) {
+                en = m;
+            } else {
+                st = m + 1;
+            }
+        }
+
+        ans = st;
+
+        if (mountainArr.get(ans) == target) {
+            return ans;
+        } else {
+
+            le = ans;
+
+           
+            while (ls <= le) {
+                int lm = ls + (le - ls) / 2;
+                int val = mountainArr.get(lm);
+
+                if (val == target) {
+                    return lm;
+                } else if (val < target) {
+                    ls = lm + 1;
+                } else {
+                    le = lm - 1;
+                }
+            }
+
+            rs = ans + 1; 
+
+            
+            while (rs <= re) {
+                int rm = rs + (re - rs) / 2;
+                int val = mountainArr.get(rm); 
+
+                if (val == target) {
+                    return rm;
+                } else if (val < target) {
+                    re = rm - 1; 
+                } else {
+                    rs = rm + 1;
+                }
+            }
+        }
+
+        return -1;
     }
-    // static int elm(int []nums, int target){
-    //     int ans=peak(nums);
-    //     if(target==ans){
-    //         return ans;
-    //     }
-    //     else if(target<ans){
-    //         int rights=
-    //     }
-    // }
-    }
+}
