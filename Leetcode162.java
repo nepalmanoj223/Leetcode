2d14aff941cdaba5b2feba98574dc2d36aab4969
Manoj Nepal <nepalmanoj223@gmail.com>
2026-03-04 14:20:20 +0545

    Add Leetcode162 solution (find peak element)
    Add Leetcode162.java with a Solution.findPeakElement implementation using binary search. The method uses a while(st < end) loop comparing arr[mid] and arr[mid+1] to converge on a peak index. Time complexity O(log n), space O(1).

---
+++ Leetcode162.java
@@ -0,0 +1,19 @@
+public class Leetcode162 {
+    class Solution {
+    public int findPeakElement(int[] arr) {
+
+        int st=0;
+        int end=arr.length-1;
+        while(st<end){
+            int mid=st+(end-st)/2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+        return st;
+    }}
+
+}
